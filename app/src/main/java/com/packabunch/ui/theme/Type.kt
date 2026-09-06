69c4dc9c2bc0c7b87bb341658b3c572847a3dd9a
DrGitman <orilionaobeb@gmail.com>
2026-09-06 11:14:43 +0200

    Refactor code structure for improved readability and maintainability

--- app/src/main/java/com/packabunch/ui/theme/Type.kt
+++ app/src/main/java/com/packabunch/ui/theme/Type.kt
@@ -1,5 +1,8 @@
+@file:OptIn(ExperimentalTextApi::class)
+
 package com.packabunch.ui.theme
 
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
